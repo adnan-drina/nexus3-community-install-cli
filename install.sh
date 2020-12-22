319e9edc8743a32f6520f84a6881307ba7497e4f
adnan.drina@gmail.com <adnan.drina@gmail.com>
2020-12-22 17:03:20 +0100

    housekeeping

--- install.sh
+++ install.sh
@@ -5,22 +5,21 @@
 echo "Creating Nexus Subscription"
 oc apply -f nexus-sub.yaml
 echo "Nexus Subscription created!"
-wait 10
-
+echo " "
 echo "Creating Nexus Project"
 oc apply -f nexus-namespace.yaml
 echo "Nexus Project created!"
-
+echo " "
 echo "Configure Nexus"
 oc apply -f scc-volatile.yaml -n nexus-repository
 oc adm policy add-scc-to-user allow-nexus-userid-200 -z nexus3
 echo "Configure Nexus Done!"
-
+echo " "
 echo "Deploying Nexus CR"
 oc apply -f nexus-cr.yaml -n nexus-repository
 echo "Nexus CR created!"
-
-echo "Searching for available routes"
-oc get routes -n nexus
-echo "connect to the route named nexus using your browser \
-and login using admin/admin123"
+echo " "
+#echo "Searching for available routes"
+#oc get routes -n nexus
+#echo "connect to the route named nexus using your browser \
+#and login using admin/admin123"
